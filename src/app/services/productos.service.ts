a4f359679176db758326ec1e220252eb82c615d5
Jorge Blondell <jorgeblondell@gmail.com>
2020-04-13 13:16:49 -0400

    Fin de la Sección 6

---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interface/producto.interface';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+  cargando = true;
+  productos: Producto [] = [];
+
+  constructor(private http: HttpClient) {
+this.cargarProductos();
+
+   }
+
+  private cargarProductos (){
+    this.http.get('https://proyecto-jorge-angular.firebaseio.com/productos_idx.json')
+    .subscribe((resp: Producto[]) => {
+
+      console.log(resp);
+       this.productos = resp;
+       this.cargando = false;
+
+
+    });
+
+  }
+}
